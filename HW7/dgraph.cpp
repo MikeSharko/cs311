24fdde1f83e03d519e5da91ecfc4caa5b1925791
mikesharko <mike.sharko1982@gmail.com>
2019-11-16 13:09:13 -0800

    DFS algorithm with a stack class

---
+++ HW7/dgraph.cpp
@@ -0,0 +1,111 @@
+//CS311 Yoshii dgraph.cpp 
+// INSTRUCTION:
+//  Complete all the functions you listed in dgraph.h
+//  Comment the file completely using How to Comment file.
+//  Use HW6-help.docx to finish the functions.
+//-------------------------------------------------------
+
+//======================================================
+// HW#: HW6 dgraph
+// Name: Mike Sharko
+// File Type: .cpp
+//========================================================
+
+using namespace std;
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "dgraph.h"
+
+dgraph::dgraph() // initialize vertexName (blank) and visit numbers (0)    // initialize countUsed to be 0
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		Gtable[i].vertexName = ' ';
+		Gtable[i].visit = 0;
+	}
+	countUsed = 0;
+}
+
+dgraph::~dgraph()   // do we have to delete all nodes of slists in table??
+// Question: If we do not do this, will the llist destructor be called automatically??? Try it.
+{
+}
+
+void dgraph::fillTable()  // be sure to read from a specified file
+{
+  string fname;
+  char c;
+ 
+  cout << "Enter a file name: ";
+  cin >> fname;
+
+ 
+  ifstream fin (fname.c_str(), ios::in); // declare and open fname
+  // the rest is in HW6-help ---- thank you
+ 
+  
+  while (fin >> Gtable[countUsed].vertexName)  // IF I CAN READ THE NAME
+  {
+	  fin >> Gtable[countUsed].outDegree;
+	  for (int i = 1; i <= Gtable[countUsed].outDegree; i++) {
+		  fin >> c;
+		  (Gtable[countUsed].adjacentOnes).addRear(c); // this is a slist function from HW3
+	  }   
+	  countUsed++;
+  }
+  fin.close();
+ }
+
+
+void dgraph::displayGraph() // be sure to display
+{// in a really nice table format -- all columns but no unused rows
+	 
+	for (int i = 0; i < countUsed; i++)
+	{
+		cout << Gtable[i].vertexName << " ";
+		cout << Gtable[i].outDegree << " ";
+		(Gtable[i].adjacentOnes).displayAll();
+	}
+	 
+}
+
+int dgraph::findOutDegree(char V)// throws exception
+{// does not use a loop
+	int tmp = int(V) - 65; // Converting ASCII
+	if (tmp >= 0 && tmp < countUsed)
+		return Gtable[tmp].outDegree;
+	throw BadVertex(); // else throw exeption
+}
+
+slist dgraph::findAdjacency(char V)// throws exception
+{// does not use a loop
+
+	int tmp = int(V) - 65; //change V to integer V is 86
+	if (tmp >= 0 && tmp < countUsed)
+		return Gtable[tmp].adjacentOnes;
+	throw BadVertex(); // else throw exeption
+}
+
+//enters the given visit number (num) for a given vertex (v)
+//to indicate the order in which vertices were visited
+void dgraph::visit(int num, char V)
+{
+	//note: do not use loop to look for vertex
+	//convert A to slot 0, B to slot 1, etc
+
+	int temp = int(V) - 65; //change V to integer where A == 0
+	Gtable[temp].visit = num;
+}
+
+
+//returns true if a given vertex was already visited
+bool dgraph::isMarked(char V)
+{
+	//note: do not use a loop to look for a vertex
+
+	int temp = int(V) - 65;
+
+	if (Gtable[temp].visit == 0) return false;
+	else return true;
+}
